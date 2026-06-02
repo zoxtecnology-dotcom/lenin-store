1977f12220f5c0eb0f3e53c1a3aef6531c50c641
JhonRojas1997 <jfrojas1997@gmail.com>
2026-06-01 21:01:01 -0500

    fix: include tslib in nitro bundle for Vercel

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,6 @@
 export default {
   preset: "vercel",
+  // Incluir tslib en el bundle para evitar ERR_MODULE_NOT_FOUND en Vercel
+  externals: [],
+  noExternals: ["tslib", "react-remove-scroll"],
 };
